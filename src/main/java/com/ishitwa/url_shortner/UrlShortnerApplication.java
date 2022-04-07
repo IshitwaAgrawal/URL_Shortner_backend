9dab0457f7ff6716e6f1cb168d44e4df5605838b
IshitwaAgrawal <ishitwa2000@gmail.com>
2022-04-08 00:26:58 +0530

    cors fix

--- src/main/java/com/ishitwa/url_shortner/UrlShortnerApplication.java
+++ src/main/java/com/ishitwa/url_shortner/UrlShortnerApplication.java
@@ -10,6 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -46,5 +48,17 @@ public class UrlShortnerApplication {
         props.put("mail.debug", "true");
 
         return mailSender;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(
+                        "http://localhost:8080"
+                );
+            }
+        };
     }
 }
