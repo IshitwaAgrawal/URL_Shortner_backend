435f5a020541657efbbf48ea4ac05fa0dcebc889
IshitwaAgrawal <ishitwa2000@gmail.com>
2022-04-08 01:09:35 +0530

    bugs fixed

--- src/main/java/com/ishitwa/url_shortner/UrlShortnerApplication.java
+++ src/main/java/com/ishitwa/url_shortner/UrlShortnerApplication.java
@@ -14,27 +14,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Properties;
 
 
 @EnableAsync
 @SpringBootApplication
-//@EnableSwagger2
 @EnableJpaRepositories(basePackageClasses = {UserRepo.class, UrlRepo.class})
 public class UrlShortnerApplication {
     public static void main(String[] args) {
         SpringApplication.run(UrlShortnerApplication.class, args);
     }
-//    @Bean
-//    public Docket productApi() {
-//        return new Docket(DocumentationType.SWAGGER_2).select()
-//                .apis(RequestHandlerSelectors.basePackage("com.ishitwa.url_shortner")).build();
-//    }
     @Bean
     public JavaMailSender getJavaMailSender(){
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
@@ -59,7 +49,7 @@ public class UrlShortnerApplication {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**").allowedOrigins(
                         "http://localhost:8080",
-                        "https://ishitwaagrawal.github.io/Url-Shortner/"
+                        "https://ishitwaagrawal.github.io/"
                 );
             }
         };
